e72e0bb368b4ffcb17bd3450263dd709018bd4c0
eVy9-1 <laubalee9@gmail.com>
2023-06-15 13:26:50 +0200

    update

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+
+function ty() {
+    alert("Danke, ich werde mich schnellstmöglich bei Ihnen melden!")
+}
+
+// var data = document.getElementById('id').value;
+var bt = document.getElementById(betreff);
+var x = "?subject= " .
