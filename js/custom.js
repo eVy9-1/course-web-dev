dd9f80dcab8d115ccf9cc796cfafb31eed08e15d
eVy9-1 <laubalee9@gmail.com>
2023-06-15 17:29:27 +0200

    daily

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,30 @@
 
-function ty() {
-    alert("Danke, ich werde mich schnellstmöglich bei Ihnen melden!")
-}
+// function ty() {
+//     alert("Danke, ich werde mich schnellstmöglich bei Ihnen melden!")
+// }
 
 // var data = document.getElementById('id').value;
-var bt = document.getElementById(betreff);
-var x = "?subject= " .
+// function ty() {
+//     alert(document.getElementById('betreff').value);
+// }
+
+
+// alert (console.log(bt));
+
+// var x = "?subject= " .
+
+
+// function sendEmail() {
+//     var email = "timon.scheer@gmail.com"; // Die ursprüngliche E-Mail-Adresse
+//     var variableInhalt = document.getElementById('betreff').value;
+//
+//     // Den Inhalt der Variable an die E-Mail-Adresse anhängen
+//     var erweiterteEmail = email + "?subject=" + variableInhalt;
+//
+//     // Das Formular-Action-Attribut aktualisieren
+//     document.getElementById("myForm").action = "mailto:" + erweiterteEmail;
+// }
+//
+//
+// <a href="mailto:m.mustermann@domain.de?subject=Hier%20steht%20der%20Betreff">E-Mail mit Betreff</a>
+//
