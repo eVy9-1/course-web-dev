47f885e1357833d51bd93b3d0de9073808c16df9
eVy9-1 <laubalee9@gmail.com>
2023-06-16 15:49:37 +0200

    daily

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,50 @@
+function mailMe() {
+    const mymail = "timon.scheer@gmail.com";
+    var mailline = 'mailto:' + mymail;
+
+    var emailsub = document.getElementById('emailsubhtml').value;
+    var emailcc = document.getElementById('emailcchtml').value;
+    var emailbody = document.getElementById('emailbodyhtml').value;
+
+    var fullmailline = "?"
+
+    // forming subject line
+    if (emailsub !== "") {
+        fullmailline += 'subject=' + emailsub
+    }
+
+    // forming cc line
+    if (emailcc !== "") {
+        if(emailsub) {
+            fullmailline += "&"
+        }
+        fullmailline += 'cc=' + emailcc
+    }
+
+    // forming body line
+    if (emailbody !== "") {
+        if(emailsub || emailbody) {
+            fullmailline += "&"
+        }
+        fullmailline += 'body=' + emailbody
+    }
+
+    fullmailline = mailline + fullmailline;
+    // FILTER //
+    // Fragezeichen suchen und ersetzen mit: %3F
+    // Leerzeichen suchen und ersetzen mit: %20
+    fullmailline = fullmailline.replace(/ /g, '%20');
+    // Linebreaks suchen und ersetzen mit: %0D
+    fullmailline = fullmailline.replace(/(\r\n|\r|\n)/g, '%0D');
+
+    // open mailto: //
+    alert(fullmailline);
+    window.open(fullmailline, '_self');
+}
+
+
+
+
 
 // function ty() {
 //     alert("Danke, ich werde mich schnellstmöglich bei Ihnen melden!")
